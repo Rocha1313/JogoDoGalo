069b01c2160e654c938493e209912af92816f019
rocha <rjoao0112@gmail.com>
2022-11-15 12:12:31 +0000

    Player class correction

--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,31 @@
 public class Player {
-    private String playerCharacter;
+    private final String playerCharacter;
+    private int wins;
+    private int losses;
 
     public Player(String playerCharacter){
         this.playerCharacter = playerCharacter;
     }
 
     //Getter's
-    public static char getPlayerCharacter() {
+    public String getPlayerCharacter() {
         return playerCharacter;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    //Setter's
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
     }
 }
